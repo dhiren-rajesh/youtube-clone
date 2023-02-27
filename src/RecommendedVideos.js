f7862d05a1e2e64e8b540f5461da6113216b9293
dhiren rajesh <dhirenrajeshk@gmail.com>
2023-02-27 11:51:42 +0530

    Added files necessary for Video Recommendations

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,94 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard'
+
 
 function RecommendedVideos() {
   return (
     <div className='rec_vids'>
       <h1>Recommended</h1>
+      <div className='videos'>
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+        <VideoCard
+          title="Scoop Diddy Whoop"
+          views="6.9M Views"
+          timestamp="7 days ago"
+          channelImage="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX_400x400.jpg"
+          channel="Dhiren Rajesh"
+          image="https://i.kym-cdn.com/photos/images/facebook/001/367/798/73c.png"
+        />
+      </div>
     </div>
   )
 }
